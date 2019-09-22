192b4efe3b3a6d14570be40211e1a0f25b7f4997
atsuky <andreugallofre@gmail.com>
2019-09-23 00:18:59 +0200

    Extracted modal from main class
    Moved modal into a subclass to make the code cleaner, now we import the
    modal from the main class.

---
+++ frontend/src/components/MainPage/CreateItem/CreateItem.js
@@ -0,0 +1,86 @@
+import React from "react";
+import { Form, Modal, Input, Select } from 'antd';
+import '../MainPage.css'
+import api from "../../../api/api";
+import 'antd/dist/antd.css'
+
+function onChange(value) {
+console.log(`selected ${value}`);
+}
+
+function onBlur() {
+console.log('blur');
+}
+
+function onFocus() {
+console.log('focus');
+}
+
+function onSearch(val) {
+console.log('search:', val);
+}
+
+const { Option } = Select;
+
+export const CreateItemForm = Form.create({ name: 'form_in_modal' })(
+    // eslint-disable-next-line
+
+    class extends React.Component {
+      
+      constructor(props) {
+        super(props);
+       
+        this.state = {
+          isLoaded: false,
+          companies: null
+        };
+      }
+  
+      componentDidMount() {
+        api.get("/company").then(response => response.data)
+        .then((data) => {
+          this.setState({ companies: data, isLoaded: true})
+          console.log(this.state.companies)
+        })
+      }
+  
+      render() {
+        const { visible, onCancel, onCreate, form } = this.props;
+        const { getFieldDecorator } = form;
+        const { isLoaded, companies } = this.state;
+        
+        return (isLoaded ?
+          <Modal
+            visible={visible}
+            title="Add new item"
+            okText="Create"
+            onCancel={onCancel}
+            onOk={onCreate}
+          >
+            <Form layout="vertical">
+              <Form.Item label="Title">
+                {getFieldDecorator('title', {
+                  rules: [{ required: true, message: 'Please input the title of collection!' }],
+                })(<Input />)}
+              </Form.Item>
+              <Form.Item label="Description">
+                {getFieldDecorator('description')(<Input type="textarea" />)}
+              </Form.Item>
+              <Form.Item label="Donation Company" className="collection-create-form_last-form-item">
+                {getFieldDecorator('modifier', {
+                  initialValue: '',
+                })(
+                <Select showSearchn style={{ width: 200 }} initialValue="Select a person" optionFilterProp="children"
+                  onChange={onChange} onFocus={onFocus} onBlur={onBlur} onSearch={onSearch}
+                  filterOption={(input, option) => option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0 }>
+                  {companies.map(cmp => (<Option key={cmp.name} value={cmp.name}>{cmp.name}</Option>)) }
+                </Select>,
+                )}
+              </Form.Item>
+            </Form>
+          </Modal> :
+          <div>Hello world</div> // or whatever loading state you want, could be null 
+        );
+      }
+    },
+  )
--- frontend/src/components/MainPage/MainPage.js
+++ frontend/src/components/MainPage/MainPage.js
@@ -3,96 +3,13 @@ import { Layout, Menu, Icon, Table, Divider, Button,
          Form, Modal, Input, Select} from 'antd';
 import './MainPage.css'
 import ReactDOM from 'react-dom';
+import { CreateItemForm } from './CreateItem/CreateItem.js'
 import {BrowserView} from "react-device-detect";
 import api from "../../api/api";
 import 'antd/dist/antd.css'
 
 const { Header, Content, Footer } = Layout;
 const { Option } = Select;
-
-function onChange(value) {
-  console.log(`selected ${value}`);
-}
-
-function onBlur() {
-  console.log('blur');
-}
-
-function onFocus() {
-  console.log('focus');
-}
-
-function onSearch(val) {
-  console.log('search:', val);
-}
-
-const Testing = Form.create({ name: 'form_in_modal' })(
-  // eslint-disable-next-line
-
-  class extends React.Component {
-    
-    constructor(props) {
-      super(props);
-     
-      this.state = {
-        isLoaded: false,
-        companies: null
-      };
-
-
-
-    }
-
-    componentDidMount() {
-      api.get("/company").then(response => response.data)
-      .then((data) => {
-        this.setState({ companies: data, isLoaded: true})
-        console.log(this.state.companies)
-      })
-    }
-
-    render() {
-      const { visible, onCancel, onCreate, form } = this.props;
-      const { getFieldDecorator } = form;
-      const { isLoaded, companies } = this.state;
-      
-      return (isLoaded ?
-        <Modal
-          visible={visible}
-          title="Add new item"
-          okText="Create"
-          onCancel={onCancel}
-          onOk={onCreate}
-        >
-          <Form layout="vertical">
-            <Form.Item label="Title">
-              {getFieldDecorator('title', {
-                rules: [{ required: true, message: 'Please input the title of collection!' }],
-              })(<Input />)}
-            </Form.Item>
-            <Form.Item label="Description">
-              {getFieldDecorator('description')(<Input type="textarea" />)}
-            </Form.Item>
-            <Form.Item label="Donation Company" className="collection-create-form_last-form-item">
-              {getFieldDecorator('modifier', {
-                initialValue: '',
-              })(
-              <Select showSearchn style={{ width: 200 }} initialValue="Select a person" optionFilterProp="children"
-                onChange={onChange} onFocus={onFocus} onBlur={onBlur} onSearch={onSearch}
-                filterOption={(input, option) => option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0 }>
-                {companies.map(cmp => (<Option key={cmp.name} value={cmp.name}>{cmp.name}</Option>)) }
-              </Select>,
-              )}
-            </Form.Item>
-          </Form>
-        </Modal> :
-        <div>Hello world</div> // or whatever loading state you want, could be null 
-      );
-    }
-  },
-)
-
-
 export class MainPage extends Component {
 
   state = {
@@ -202,7 +119,7 @@ export class MainPage extends Component {
                         </Footer>
                     </Layout>
                 </BrowserView>
-                <Testing
+                <CreateItemForm
                   wrappedComponentRef={this.saveFormRef}
                   visible={this.state.visible}
                   onCancel={this.handleCancel}
